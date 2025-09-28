795e5681d064b376c78df27e68687194fd4c8912
Dion Lind <dion.lind22@gmail.com>
2025-09-28 16:22:18 +0200

    Initial commit - Task 1

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, status
+from utils.constants import Endpoints, ResponseMessages
+import uvicorn 
+
+
+#instance of FastAPI for voting_app
+voting_app = FastAPI(
+    title="Voting App",
+    description="A simple voting application API",
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+
+
+@voting_app.get(Endpoints.ROOT)
+def read_root():
+    """
+    This comment wil show up in /docs as well
+    """
+    return {"message": ResponseMessages.WELCOME, "status": status.HTTP_200_OK}
+
+
+@voting_app.get(Endpoints.HEALTH)
+def read_health():
+    return {"message": ResponseMessages.HEALTH_OK, "status": status.HTTP_200_OK}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:voting_app", host="0.0.0.0", port=8000, reload =True)
---
+++ utils/constants.py
@@ -0,0 +1,8 @@
+class Endpoints:
+    ROOT = "/"
+    HEALTH = "/health"
+
+
+class ResponseMessages:
+    WELCOME = "Welcome to the Voting App!!"
+    HEALTH_OK = "The service is up and running(YAY)!"
